9b58e44413f2aa9f0b4708e23bb29f3e7fd225ee
Moises Daboin <54545246+DeviousMeister@users.noreply.github.com>
2026-09-16 17:37:33 -0600

    testings this again

--- config/aws-config.js
+++ config/aws-config.js
@@ -3,8 +3,8 @@
 
 const awsConfig = {
   region: "us-east-1",
-  accessKeyId: "AKIA2E0A8F3B9C1D4E5F",
-  secretAccessKey: "***REMOVED***",
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
   bucket: "devsecops-fall2026-backups",
 };
 
